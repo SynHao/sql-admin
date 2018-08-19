c87da110832d236aa2bd39fc2acc0bfcfb8f4e7d
haoshuaiyu(蒿帅雨.大数据中心.大数据产品部高级产品经理) <haoshuaiyu@gomeplus.com>
2018-08-19 23:22:56 +0800

    搞了个布局

---
+++ src/Router.tsx
@@ -0,0 +1,18 @@
+// import * as React from 'react';
+// import {Switch, Redirect, BrowserRouter as Router} from 'react-router-dom';
+
+
+// class ContentRouter extends React.Component<any> {
+//     render() {
+//         <Router basename="/">
+//             <div id="router">
+//                 <Switch>
+//                     <Redirect exact={true} from="/" to="/database?tab=datamodel" />
+//                     {/* <Route path="/" render={appRender} /> */}
+//                 </Switch>
+//             </div>
+//       </Router>
+//     }
+// }
+
+// export default ContentRouter;
